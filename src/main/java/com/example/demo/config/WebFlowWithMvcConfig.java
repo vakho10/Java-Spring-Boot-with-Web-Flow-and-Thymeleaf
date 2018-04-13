d16609be6611305c297a008c1a0caeaabe0642eb
CRA\v.laluashvili <vakho10@gmail.com>
2018-04-13 15:39:42 +0400

    Refactored and removed unncessary configs and files.

---
+++ src/main/java/com/example/demo/config/WebFlowWithMvcConfig.java
@@ -0,0 +1,101 @@
+package com.example.demo.config;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.webflow.config.AbstractFlowConfiguration;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.engine.builder.ViewFactoryCreator;
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
+import org.springframework.webflow.executor.FlowExecutor;
+import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
+import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.webflow.view.AjaxThymeleafViewResolver;
+import org.thymeleaf.spring5.webflow.view.FlowAjaxThymeleafView;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class WebFlowWithMvcConfig extends AbstractFlowConfiguration {
+
+	@Bean
+	public FlowDefinitionRegistry flowRegistry() {
+		return getFlowDefinitionRegistryBuilder() //
+				.setBasePath("/WEB-INF/flows") //
+				.addFlowLocationPattern("/**/*-flow.xml") //
+				.setFlowBuilderServices(this.flowBuilderServices()) //
+				.build();
+	}
+
+	@Bean
+	public FlowExecutor flowExecutor() {
+		return getFlowExecutorBuilder(this.flowRegistry()) //
+				.build();
+	}
+
+	@Bean
+	public FlowBuilderServices flowBuilderServices() {
+		return getFlowBuilderServicesBuilder() //
+				.setViewFactoryCreator(this.mvcViewFactoryCreator()) // Important!
+				.build();
+	}
+	// ----------------------------------------------------------
+	
+	@Bean
+	public FlowHandlerMapping flowHandlerMapping() {
+		FlowHandlerMapping handlerMapping = new FlowHandlerMapping();
+		handlerMapping.setOrder(-1);
+		handlerMapping.setFlowRegistry(this.flowRegistry());
+		return handlerMapping;
+	}
+
+	@Bean
+	public FlowHandlerAdapter flowHandlerAdapter() {
+		FlowHandlerAdapter handlerAdapter = new FlowHandlerAdapter();
+		handlerAdapter.setFlowExecutor(this.flowExecutor());
+		handlerAdapter.setSaveOutputToFlashScopeOnRedirect(true);
+		return handlerAdapter;
+	}
+
+	@Bean
+	public ViewFactoryCreator mvcViewFactoryCreator() {
+		MvcViewFactoryCreator factoryCreator = new MvcViewFactoryCreator();
+		factoryCreator.setViewResolvers(Collections.singletonList(this.thymeleafViewResolver()));
+		factoryCreator.setUseSpringBeanBinding(true);
+		return factoryCreator;
+	}
+
+	@Bean
+	@Description("Thymeleaf AJAX view resolver for Spring WebFlow")
+	public AjaxThymeleafViewResolver thymeleafViewResolver() {
+		AjaxThymeleafViewResolver viewResolver = new AjaxThymeleafViewResolver();
+		viewResolver.setViewClass(FlowAjaxThymeleafView.class);
+		viewResolver.setTemplateEngine(this.templateEngine());
+		viewResolver.setCharacterEncoding("UTF-8");
+		return viewResolver;
+	}
+
+	@Bean
+	@Description("Thymeleaf template resolver serving HTML 5")
+	public ClassLoaderTemplateResolver templateResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setPrefix("templates/");
+		templateResolver.setCacheable(false);
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setCharacterEncoding("UTF-8");
+		return templateResolver;
+	}
+
+	@Bean
+	@Description("Thymeleaf template engine with Spring integration")
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(this.templateResolver());
+		return templateEngine;
+	}	
+}
