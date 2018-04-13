b7f4713db9192eac107da75c3cda42a7cecc6fae
CRA\v.laluashvili <vakho10@gmail.com>
2018-04-13 14:54:01 +0400

    First commit.

---
+++ src/main/java/com/example/demo/Blog1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog1Application.class, args);
+	}
+}
