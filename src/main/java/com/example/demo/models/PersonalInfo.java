603c74dc66ba96fcf460aec175b4d7dd091e6f9d
vakho <vakho10@gmail.com>
2018-04-15 21:33:31 +0400

    Added image upload functionality.

--- src/main/java/com/example/demo/models/PersonalInfo.java
+++ src/main/java/com/example/demo/models/PersonalInfo.java
@@ -1,12 +1,18 @@
 package com.example.demo.models;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Base64;
 
 import javax.validation.constraints.NotBlank;
+
+import org.springframework.web.multipart.MultipartFile;
 
 public class PersonalInfo implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	private byte[] imageContent;
 
 	@NotBlank
 	private String username;
@@ -22,6 +28,18 @@ public class PersonalInfo implements Serializable {
 
 	@NotBlank
 	private String lastname;
+
+	public byte[] getImageContent() {
+		return imageContent;
+	}
+
+	public String getImageBase64() {
+		return Base64.getEncoder().encodeToString(imageContent);
+	}
+
+	public void setImage(MultipartFile image) throws IOException {
+		this.imageContent = image.getBytes();
+	}
 
 	public String getUsername() {
 		return username;
